a769a302227aca5111673a3bd7bcc7f418f4134a
valeriy <walyas89@gmail.com>
2020-01-29 08:10:52 +0200

    time fix

--- src/main/java/com/example/testingbot/constant/Admin.java
+++ src/main/java/com/example/testingbot/constant/Admin.java
@@ -2,10 +2,10 @@ package com.example.testingbot.constant;
 
 public enum Admin {
 
-    //    TOKEN("997367876:AAGFZVw0c81zUQIOrXeKhuUz0MDt6f8roJU"),
-//    BOT_NAME("company_testing_bot"),
-    TOKEN("920411958:AAEjN4VyKGZYkoKZalvdIfhD0PROnFGtwL0"),
-    BOT_NAME("dating_zp_bot"),
+        TOKEN("997367876:AAGFZVw0c81zUQIOrXeKhuUz0MDt6f8roJU"),
+    BOT_NAME("company_testing_bot"),
+//    TOKEN("920411958:AAEjN4VyKGZYkoKZalvdIfhD0PROnFGtwL0"),
+//    BOT_NAME("dating_zp_bot"),
     ADMIN_ID_3("370678219"),
     ADMIN_ID("20419906"),
     ADMIN_ID_2("654323"),
