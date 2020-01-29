5daafa4a8344f44c5c1bfd21e37d9d3ec03051ed
valeriy <walyas89@gmail.com>
2020-01-29 05:52:12 +0200

    added photo from telegram

--- src/main/java/com/example/testingbot/constant/Admin.java
+++ src/main/java/com/example/testingbot/constant/Admin.java
@@ -4,7 +4,11 @@ public enum Admin {
 
     TOKEN("997367876:AAGFZVw0c81zUQIOrXeKhuUz0MDt6f8roJU"),
     BOT_NAME("company_testing_bot"),
-    ADMIN_ID("370678219"),
+//    TOKEN("920411958:AAEjN4VyKGZYkoKZalvdIfhD0PROnFGtwL0"),
+//    BOT_NAME("dating_zp_bot"),
+    //ADMIN_ID("370678219"),
+    //ADMIN_ID("20419906"),
+    ADMIN_ID("654323"),
     //ADMIN_ID("11111111"),
     BASIC_URL("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"),
     BASIC_URL_PHOTO("https://api.telegram.org/bot%s/sendPhoto?chat_id=%s&photo=%s");
